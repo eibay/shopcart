952f568d4ceca7608429af3fe962467020d95af9
Elmer Ibayan <eibayan@gmail.com>
2017-04-22 16:42:36 +1000

    add product clickable row feature and style

--- app/assets/javascripts/detail.js
+++ app/assets/javascripts/detail.js
@@ -1,6 +1,6 @@
 window.selectProduct = function() {
 $('.products-row').on('click', function(){
     itemClick = $(this).attr("id");
-    window.location = ROUTES.PRODUCT_PATH + "/" + itemClick;
+    window.location = ROUTES.PRODUCT_PATH + "/" + (parseInt(itemClick) + 1);
   });
 };
