7568dcc0a2d6190852bf704ebfe088e0472cae77
Elmer Ibayan <eibayan@gmail.com>
2017-04-22 18:56:58 +1000

    Add product tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -2,15 +2,28 @@ require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
 
-test "product attributes must not be empty" do
-  # test "the truth" do
-  # assert true
-  # end
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:name].any?
+    assert product.errors[:unit_price].any?
+  end
 
-  # product = Product.new
-  # assert product.invalid?
-  # assert product.errors[:name].any?
-  # assert product.errors[:unit_price].any?
-  # assert product.errors[:special_qty].any?
-  # assert product.errors[:special_price].any?
+  test "product unit price must be positive" do 
+    product = Product.new(name: "Anime")
+    
+    product.unit_price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+      product.errors[:unit_price]
+
+    product.unit_price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+      product.errors[:unit_price]
+
+    product.unit_price = 1
+    assert product.valid?
+  end
+
 end
