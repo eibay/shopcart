81f3478cc40ed0e4021a16ebe39669afae3cd51c
Elmer Ibayan <eibayan@gmail.com>
2017-04-23 07:11:36 +1000

    generate lineitem scaffolds

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,15 @@ class Product < ApplicationRecord
   validates :name, :unit_price, presence:true
   validates :unit_price, numericality: {greater_than_or_equal_to: 0.01}
   validates :name, uniqueness: true
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, 'Line Items Present')
+      throw :abort
+  end
+
 end
+
