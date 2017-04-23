2b080f91ae5b36a3c0aebd992779af13aa9278fd
Elmer Ibayan <eibayan@gmail.com>
2017-04-23 11:22:53 +1000

    add line-items for cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,11 +6,14 @@ class Product < ApplicationRecord
   before_destroy :ensure_not_referenced_by_any_line_item
 
   private
+
   def ensure_not_referenced_by_any_line_item
     unless line_items.empty?
       errors.add(:base, 'Line Items Present')
       throw :abort
+    end
   end
-
+  
 end
+
 
